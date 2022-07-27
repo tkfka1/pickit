6cea410a070292a1c2b9b23cce70e65c86015bcc
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2022-07-28 04:51:29 +0900

    룬

--- src/module/ddKey.py
+++ src/module/ddKey.py
@@ -1,10 +1,4 @@
 from ctypes import *
+import time
 
-dd_dll = windll.LoadLibrary('src/DD94687.64.dll')
-
-st = dd_dll.DD_btn(0) #classdd 초기설정
-if st==1:
-    print("OK")
-else:
-    print("Error")
-    exit(101)
+print("Load DD!")
