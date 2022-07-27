447e703a272e184f98fbb3be57e9f3fc42a3a3ed
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2022-07-28 05:41:41 +0900

    오쇼

--- src/module/pyduino.py
+++ src/module/pyduino.py
@@ -1,4 +1,22 @@
 from pyduino_mk.arduino import Arduino
-from pyduino_mk.constants import *
-arduino = Arduino()
-arduino = Arduino(port='COM3')  # Windows example
+from pyduino_mk.arduino import *
+import time
+
+ardu = Arduino()
+time.sleep(1)
+ardu.move(300, 300)
+time.sleep(3)
+# ardu.type('https://github.com/nelsontran/PyDuino-MK/', accuracy=85)
+ardu.write('A')
+ardu.write(65)
+
+# Hit Ctrl+Alt+Delete
+# arduino.press(LEFT_CTRL)
+# arduino.press(LEFT_ALT)
+# arduino.press(DELETE)
+# arduino.releaseAll()
+
+# Drag the left mouse button to (500, 500)
+# arduino.press()
+# arduino.bezier_move(500, 500)
+# arduino.release()
